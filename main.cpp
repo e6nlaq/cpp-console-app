58075508077d9026c18475081fb7413887a50104
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2022-12-18 08:20:32 +0900

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,22 @@
 
 #include <bits/stdc++.h>
+using namespace std;
+
+#define co(x) cout << (x) << endl;
+
+int main(int argc, char const *argv[])
+{
+	string in;
+	while (true)
+	{
+		cin >> in;
+		if (in == "exit")
+			break;
+		else
+		{
+			co("Error: " + in + "は無効です");
+		}
+	}
+
+	return 0;
+}
